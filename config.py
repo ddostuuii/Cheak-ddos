92afcfaeaff8e0767728dbe7acd42ef672c60613
ddostuuii <stdjtest@gmail.com>
2025-02-04 12:26:22 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = '7711360792:AAE9G4vqiak1URRYY9gBUj1_xdchTxU7usk'  # Replace with your actual bot token  
+ADMIN_IDS = ['7017469802']  # Replace with actual Telegram user IDs of admins  
+OWNER_USERNAME = '@seedhe_maut'  # Replace with your Telegram username  
